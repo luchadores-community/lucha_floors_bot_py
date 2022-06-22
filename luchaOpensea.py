8afacc57b7d2fac1a9b5d1e91980bd1557b18c3e
Baptiste PREALPATO <bpdiv@pm.me>
2022-06-22 10:07:35 +0200

    debug div class

--- luchaOpensea.py
+++ luchaOpensea.py
@@ -22,7 +22,7 @@ class OpenseaQuerries:
             return
         bswebpage = BeautifulSoup(resp.text, "html.parser")
 
-        for assetCardFooter in bswebpage.findAll('div',{'class':'AssetCardFooter--price'}):
+        for assetCardFooter in bswebpage.findAll('div',{'class':'sc-1xf18x6-0 sc-1twd32i-0 sc-1wwz3hp-0 xGokL kKpYwv kuGBEl'}):
             for assetCardFooterPriceAmount in assetCardFooter.findAll('div', {'class':'AssetCardFooter--price-amount'}):
                 floor = assetCardFooterPriceAmount.findAll('div', {'class':'Price--amount'})[0].text.strip()
                 print("-- floor = " + floor)
